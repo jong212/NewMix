166e6d7200bc9996be0c0b86c38fc72c1caf2726
jonghwa <jonghwa0212@gmail.com>
2024-10-09 23:20:26 +0900

    [UPDATE] 주석제거2

--- Assets/Scripts/Networking/PlayerSpawner.cs
+++ Assets/Scripts/Networking/PlayerSpawner.cs
@@ -1,73 +1,86 @@
 using Fusion;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined, IPlayerLeft
 {
     public event Action<PlayerRef> OnPlayerJoined;
     public event Action<PlayerRef> OnPlayerLeft;
+
     [field: SerializeField] public GameObject PlayerPrefab { get; private set; }
 
     public void PlayerJoined(PlayerRef player)
-	{
-		InterfaceManager.instance.PrintPlayerCount(Runner.SessionInfo.PlayerCount, Runner.SessionInfo.MaxPlayers);
+    {
+        InterfaceManager.instance.PrintPlayerCount(Runner.SessionInfo.PlayerCount, Runner.SessionInfo.MaxPlayers);
 
+        // Start the spawn process for all players (including the local one)
         if (player == Runner.LocalPlayer)
-		{
-			StartCoroutine(SpawnRoutine());
+        {
+            StartCoroutine(SpawnRoutine(player));
+        }
+    }
 
+    IEnumerator SpawnRoutine(PlayerRef player)
+    {
+        yield return new WaitUntil(() => GameManager.instance != null);
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => UIScreen.activeScreen == InterfaceManager.instance.gameplayHUD);
+
+        if (SpawnpointManager.GetSpawnpoint(out Vector3 location, out Quaternion orientation))
+        {
+            Debug.Log("Spawning player");
+            Runner.SpawnAsync(
+                prefab: PlayerPrefab,
+                position: location,
+                rotation: orientation,
+                inputAuthority: player,
+                onCompleted: (res) =>
+                {
+                    if (res.IsSpawned)
+                    {
+                        Runner.SetPlayerObject(player, res.Object);
+
+                        // Send the player info to the master client using a static RPC
+                        RPC_NotifyMasterClient(Runner, player, res.Object.GetComponent<NetworkObject>());
+                    }
+                }
+            );
         }
         else
-		{
-            OnPlayerJoined?.Invoke(player);
+        {
+            Debug.LogWarning("Unable to spawn player");
+        }
+    }
 
-            InterfaceManager.instance.ChefIconShake();
-		}
+    // Use a static RPC to notify the master client with player information
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public static void RPC_NotifyMasterClient(NetworkRunner runner, PlayerRef player, NetworkObject playerObject)
+    {
+        if (runner.IsSharedModeMasterClient)
+        {
+            Debug.Log($"Master client received player info: {player}");
+            PlayerSpawner instance = FindObjectOfType<PlayerSpawner>();
+            if (instance != null)
+            {
+                instance.HandlePlayerJoined(player);
+            }
+        }
+    }
+    public void HandlePlayerJoined(PlayerRef player)
+    {
+        OnPlayerJoined?.Invoke(player);
+    }
 
-		IEnumerator SpawnRoutine()
-		{
-			//yield return new WaitUntil(() => SpawnpointManager.Instance != null);
-			yield return new WaitUntil(() => GameManager.instance != null);
+    public void PlayerLeft(PlayerRef player)
+    {
+        InterfaceManager.instance.PrintPlayerCount(Runner.SessionInfo.PlayerCount, Runner.SessionInfo.MaxPlayers);
 
-			//UIScreen.Focus(InterfaceManager.instance.playerSettingScreen);
-
-			yield return new WaitForEndOfFrame();
-
-			//GameManager.instance.ReservedPlayerVisualsChanged();
-
-			yield return new WaitUntil(() => UIScreen.activeScreen == InterfaceManager.instance.gameplayHUD);
-
-			if (SpawnpointManager.GetSpawnpoint(out Vector3 location, out Quaternion orientation))
-			{
-				Debug.Log("Spawning player");
-				Runner.SpawnAsync(
-					prefab: PlayerPrefab,
-					position: location,
-					rotation: orientation,
-					inputAuthority: player,
-					onCompleted: (res) => { if (res.IsSpawned) Runner.SetPlayerObject(Runner.LocalPlayer, res.Object);
-                        OnPlayerJoined?.Invoke(Runner.LocalPlayer); // 스폰 완료 후에 이벤트 호출
-
-                    }
-                );
-			}
-			else
-			{
-				Debug.LogWarning("Unable to spawn player");
-			}
-		}
-	}
-
-	public void PlayerLeft(PlayerRef player)
-	{
-		InterfaceManager.instance.PrintPlayerCount(Runner.SessionInfo.PlayerCount, Runner.SessionInfo.MaxPlayers);
-		//GameManager.instance.ReservedPlayerVisualsChanged();
-		if (Runner.IsSharedModeMasterClient)
-		{
-			Debug.Log("LeftTest : " + "1");
+        if (Runner.IsSharedModeMasterClient)
+        {
+            Debug.Log("Master client handling player left: " + player);
             OnPlayerLeft?.Invoke(player);
+            // Remove player from any tracked lists or states if necessary
         }
     }
 }
